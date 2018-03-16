715c3d13b0939bc3b5672caf53c4c266c2204824
Eray Adıyoklar <erayadiyoklar@gmail.com>
2018-03-16 17:26:57 +0300

    Lab - Python

---
+++ 3.Hafta.py
@@ -0,0 +1,11 @@
+import sys
+my_L_1 = []
+size_1 = sys.getsizeof(my_L_1)
+print("Başlangıç Boyutu: " , size_1)
+for i in range(10):
+    my_L_1.append(str(i+1) + ".test")
+    if(size_1 != sys.getsizeof(my_L_1)):
+        print("Dizi yer değiştirdi" , end=" ~~ ")
+        size_1 = sys.getsizeof(my_L_1)
+        print("Size: " , size_1)
+print(my_L_1)
